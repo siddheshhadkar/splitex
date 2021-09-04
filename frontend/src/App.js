48ffdb93f419bdedb70d75d75c12842a949b73c3
Aarushi Apte <aarushiapte99@gmail.com>
2021-09-04 19:46:46 +0530

    Landing page w/ navbar done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,17 @@
+import LandingPage from "./components/LandingPage";
+import {Switch,Route, BrowserRouter as Router} from 'react-router-dom';
+
 function App() {
-  return <div>SPLITEX</div>;
+  return (
+    <div>
+      <Router>
+        <Switch>         
+          <Route exact path = "/" component = {LandingPage}></Route>
+       </Switch>
+      </Router>
+      </div>
+
+  ) 
 }
 
 export default App;
---
+++ frontend/src/components/LandingNavbar.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import {Navbar, Container, Button} from 'react-bootstrap'
+import '../styles/landingNavbar.css'
+
+export default function LandingNavbar() {
+    return (
+      <>
+        <Navbar bg="light">
+          <Container>
+            <Navbar.Brand
+              href=""
+              className="brandName"
+              style={{ color: "#3B0A79" }}
+            >
+              {" "}
+              SplitEx{" "}
+            </Navbar.Brand>
+          </Container>
+
+          <Button href="#" className="btn">
+            {" "}
+            Login{" "}
+          </Button>
+          
+          <Button href="#" className="btn" style={{ marginRight: 50 }}>
+            {" "}
+            SignUp{" "}
+          </Button>
+        </Navbar>
+      </>
+    );
+}
