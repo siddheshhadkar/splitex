1f195a6b9902e7e3a60899a1144f9e82dde22c0e
priyanshramnani <priyansh.ramnani@quantiphi.com>
2021-09-07 14:10:46 +0530

    Converted all elements of Dashboard page into Components

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -1,7 +1,12 @@
-import React from 'react'
-import { Container, Row, Col, Button, Card } from 'react-bootstrap'
-import LandingNavbar from './LandingNavbar'
-import '../styles/dashboard.css'
+import React from 'react';
+import { Container, Row, Col, Button, Card } from 'react-bootstrap';
+import LandingNavbar from './LandingNavbar';
+import '../styles/dashboard.css';
+import YouOweCard from './YouOweCard';
+import YouAreOwedCard from './YouAreOwedCard';
+import TransactionHistoryCard from './TransactionHistoryCard';
+import FriendsCard from './FriendsCard';
+import BalanceCard from './BalanceCard';
 
 export default function Dashboard() {
     return (
@@ -20,93 +25,13 @@ export default function Dashboard() {
                 </Row>
                 <Row>
                     <Col xs={12} md={4}>
-                        <Card style={{ width: '25rem', height: '25rem' }}>
-                            <Card.Body>
-                                <Card.Title className="card-title">You owe
-                                </Card.Title>
-                                <Card style={{ width: '23rem', height: '5.5rem' }}>
-                                    <Card.Body>
-                                        <Card.Text className="owe-card-text">
-                                            <strong> Monali </strong> <br />
-                                            Lunch <br />
-                                            Rs. 400
-                                            <div className="pay-button-div">
-                                                <button className="pay-button">Pay</button>
-                                            </div>
-                                        </Card.Text>
-                                    </Card.Body>
-                                </Card>
-                                <br />
-                                <Card style={{ width: '23rem', height: '5.5rem' }}>
-                                    <Card.Body>
-                                        <Card.Text className="owe-card-text">
-                                            <strong> Aarushi </strong> <br />
-                                            Dinner <br />
-                                            Rs. 500
-                                            <div className="pay-button-div">
-                                                <button className="pay-button">Pay</button>
-                                            </div>
-                                        </Card.Text>
-                                    </Card.Body>
-                                </Card>
-                                <br />
-                                <Card style={{ width: '23rem', height: '5.5rem' }}>
-                                    <Card.Body>
-                                        <Card.Text className="owe-card-text">
-                                            <strong>  Monali </strong> <br />
-                                            Groceries <br />
-                                            Rs. 200
-                                            <div className="pay-button-div">
-                                                <button className="pay-button">Pay</button>
-                                            </div>
-                                        </Card.Text>
-                                    </Card.Body>
-                                </Card>
-                            </Card.Body>
-                        </Card>
+                        <YouOweCard />
                     </Col>
                     <Col xs={12} md={4}>
-                        <Card style={{ width: '25rem', height: '25rem' }}>
-                            <Card.Body>
-                                <Card.Title className="card-title">You are owed</Card.Title>
-                                <Card style={{ width: '23rem', height: '5.5rem' }}>
-                                    <Card.Body>
-                                        <Card.Text className="owed-card-text">
-                                            Siddhesh
-                                            <div className="owed-card-amount">
-                                                Rs. 400
-                                            </div>
-                                        </Card.Text>
-                                    </Card.Body>
-                                </Card>
-                                <br />
-                                <Card style={{ width: '23rem', height: '5.5rem' }}>
-                                    <Card.Body>
-                                        <Card.Text className="owed-card-text">
-                                            Priyansh
-                                            <div className="owed-card-amount">
-                                                Rs. 400
-                                            </div>
-                                        </Card.Text>
-                                    </Card.Body>
-                                </Card>
-                                <br />
-
-                            </Card.Body>
-                        </Card>
+                        <YouAreOwedCard />
                     </Col>
                     <Col xs={12} md={4}>
-                        <Card style={{ width: '25rem', height: '25rem' }}>
-                            <Card.Body>
-                                <Card.Title className="right-section-card-title">Your balance</Card.Title>
-                                <hr className="title-separator" />
-                                <Button className="add-balance-btn"> Add Balance</Button>
-
-                                <Card.Text className="balance-div">
-                                    Rs. 1000
-                                </Card.Text>
-                            </Card.Body>
-                        </Card>
+                        <BalanceCard />
                     </Col>
                 </Row>
 
@@ -121,49 +46,13 @@ export default function Dashboard() {
                 </Row>
                 <Row>
                     <Col xs={12} md={8}>
-
-                        <Card style={{ width: '53rem', height: '5rem' }}>
-                            <Card.Body>
-                                <Card.Title className="transaction-card-body">
-
-                                    Siddesh paid you Rs.200
-                                </Card.Title>
-                                <Card.Text>
-                                    <div className="transaction-card-date">September 2, 2021</div>
-
-                                </Card.Text>
-                                <br />
-                            </Card.Body>
-                        </Card>
-                        <br />
-                        <Card style={{ width: '53rem', height: '5rem' }}>
-                            <Card.Body>
-                                <Card.Title className="transaction-card-body">You paid Aarushi Rs.200
-                                </Card.Title>
-
-                                <Card.Text>
-                                    <div className="transaction-card-date">September 2, 2021</div>
-
-                                </Card.Text>
-                            </Card.Body>
-                        </Card>
+                        <TransactionHistoryCard />
                     </Col>
 
 
                     <Col xs={12} md={4}>
                         <br />
-                        <Card className="friends-card" style={{ width: '25rem', height: '20rem' }}>
-                            <Card.Body>
-                                <Card.Title className="right-section-card-title">Friends</Card.Title>
-                                <hr className="title-separator" />
-                                <Card.Text className="friends-text">
-                                    Aarushi <br /> <hr />
-                                    Monali <br /> <hr />
-                                    Priyansh <br /> <hr />
-                                    Siddhesh <br />
-                                </Card.Text>
-                            </Card.Body>
-                        </Card>
+                        <FriendsCard />
                     </Col>
                 </Row>
 
--- frontend/src/components/LandingPage.js
+++ frontend/src/components/LandingPage.js
@@ -1,26 +1,26 @@
-import React from 'react'
-import LandingNavbar from './LandingNavbar'
-import '../styles/landingPage.css'
-import heroImage from '../assets/heroImage.svg'
+import React from 'react';
+import LandingNavbar from './LandingNavbar';
+import '../styles/landingPage.css';
+import heroImage from '../assets/heroImage.svg';
 
 function LandingPage() {
-    return (
-      <>
-        <LandingNavbar />
-        <body>
-          <img className="svgImage" src={heroImage} alt="SVG Image" />
+  return (
+    <>
+      <LandingNavbar />
+      <body>
+        <img className="svgImage" src={heroImage} alt="SVG Image" />
 
-          <div className="heading">
-            Relieve yourself from the hassle <br /> of remembering bills shared!{" "}
-            <br /> <br />
-            <div className="subHeading">
-              Introducing SplitEx, a complete <br /> solution for all your bill
-              splitting needs!
-            </div>{" "}
-          </div>
-        </body>
-      </>
-    );
+        <div className="heading">
+          Relieve yourself from the hassle <br /> of remembering bills shared!{" "}
+          <br /> <br />
+          <div className="subHeading">
+            Introducing SplitEx, a complete <br /> solution for all your bill
+            splitting needs!
+          </div>{" "}
+        </div>
+      </body>
+    </>
+  );
 }
 
 export default LandingPage
