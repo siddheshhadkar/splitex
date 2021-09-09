b59a5af2c8532543a65ac2f9b252fd18481cc810
siddheshhadkar <siddheshhadkar07@gmail.com>
2021-09-09 09:21:32 +0530

    Made landing page responsive

--- frontend/src/components/LandingPage.js
+++ frontend/src/components/LandingPage.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import LandingNavbar from './LandingNavbar';
-import '../styles/landingPage.css';
-import heroImage from '../assets/heroImage.svg';
+import React from "react";
+import LandingNavbar from "./LandingNavbar";
+import "../styles/landingPage.css";
+import heroImage from "../assets/heroImage.svg";
+import { Col, Container, Image, Row } from "react-bootstrap";
 
 function LandingPage() {
   return (
     <>
       <LandingNavbar />
-      <body>
-        <img className="svgImage" src={heroImage} alt="SVG Image" />
-
-        <div className="heading">
-          Relieve yourself from the hassle <br /> of remembering bills shared!{" "}
-          <br /> <br />
-          <div className="subHeading">
-            Introducing SplitEx, a complete <br /> solution for all your bill
-            splitting needs!
-          </div>{" "}
-        </div>
-      </body>
+      <Container fluid="md" className="mt-5">
+        <Row className="h-100">
+          <Col xs={12} sm={6} className="my-auto">
+            <div className="">
+              <h1>
+                Relieve yourself from the hassle of remembering bills shared!
+              </h1>
+              <br />
+              <br />
+              <div className="">
+                <p className="lead">
+                  Introducing SplitEx, a complete <br /> solution for all your
+                  bill splitting needs!
+                </p>
+              </div>
+            </div>
+          </Col>
+          <Col xs={12} sm={6}>
+            <Image className="" src={heroImage} alt="SVG" fluid />
+          </Col>
+        </Row>
+      </Container>
     </>
   );
 }
 
-export default LandingPage
+export default LandingPage;
